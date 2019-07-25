fac49a865c6eeac061a89bede4d1d002f97b4afa
BlasMorelBarboza <blas.morel.barboza@gmail.com>
2019-07-25 14:17:24 -0400

    Agregando Form Productos

--- BMFarma/src/FORMULARIOS/ProductosExpres.java
+++ BMFarma/src/FORMULARIOS/ProductosExpres.java
@@ -40,6 +40,7 @@ public class ProductosExpres extends javax.swing.JFrame {
         jTextField3 = new javax.swing.JTextField();
         jLabel15 = new javax.swing.JLabel();
         jTextField5 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel16 = new javax.swing.JLabel();
@@ -73,6 +74,8 @@ public class ProductosExpres extends javax.swing.JFrame {
 
         jTextField5.setText("jTextField5");
 
+        jLabel5.setText("nose");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -95,7 +98,9 @@ public class ProductosExpres extends javax.swing.JFrame {
                                     .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING)
                                     .addComponent(jLabel10, javax.swing.GroupLayout.Alignment.TRAILING)
                                     .addComponent(jLabel11, javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel12, javax.swing.GroupLayout.Alignment.TRAILING))))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jLabel5)
+                                        .addComponent(jLabel12)))))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE)
@@ -133,7 +138,9 @@ public class ProductosExpres extends javax.swing.JFrame {
                 .addComponent(jLabel11)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel12)
-                .addContainerGap(277, Short.MAX_VALUE))
+                .addGap(57, 57, 57)
+                .addComponent(jLabel5)
+                .addContainerGap(206, Short.MAX_VALUE))
         );
 
         jLabel14.setText("COD ITEM");
@@ -249,6 +256,7 @@ public class ProductosExpres extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField1;
